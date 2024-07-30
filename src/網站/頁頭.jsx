b9c2b197352bb7d4a1683528ae74ec2c1747b0dc
Bunli <bunli@ithuan.tw>
2024-07-30 10:23:34 +0800

    * 站頂提示落線日子。

--- src/網站/頁頭.jsx
+++ src/網站/頁頭.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import 'semantic-ui-css/components/message.min.css';
 
 const 頁頭 = () => (
-  <div className='app block'/>
+  <div className='app block'>
+    <div className="ui blue message">
+      由於本服務後端合成技術過舊，已不易維護，預計2024/8/29（四）下線，感謝對本服務的愛護與諒解。
+    </div>
+  </div>
 );
 
 export default 頁頭;
